f818371b4f7313a9ee30a3aaf1ad609436df66b3
Renan Saccon <sacconrenan@gmail.com>
2024-06-26 00:38:14 -0300

    Inicial Commit

---
+++ app/src/main/java/saccon/renan/br/tiptime/MainActivity.kt
@@ -0,0 +1,16 @@
+package saccon.renan.br.tiptime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
